36f61c3edb8eae2df3be9537feb58275a1c709a9
LostHuman1 <67455389+LostHuman1@users.noreply.github.com>
2020-09-07 13:26:20 +0545

    Added Character Movement and Character Switching mechanics

---
+++ New Unity Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform player;
+
+    private float mouseX;
+    private float mouseSensitivity = 100.0f;
+
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 10.0f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+		{
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		}
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+    }
+}
